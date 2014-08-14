e6989f1cb3c889e562f1e7ffc15949b4dbda3d04
Samuel Durkin <sam.thomas.durkin@gmail.com>
2014-08-14 20:49:06 +0100

    Refactor recipe creation in spec

--- spec/features/recipes/create_spec.rb
+++ spec/features/recipes/create_spec.rb
@@ -1,39 +1,90 @@
 require 'spec_helper'
 
+
 describe "Creating recipes" do
-  it "redirects to the recipe index page on success" do
+  def create_recipe(options={})
+    options[:title] ||="Koppi"
+    options[:coffee] ||="18"
+    options[:water] ||="220"
+    options[:seconds] ||="90"
+    options[:description] ||="My everyday coffee recipe"
+
     visit "/recipes"
     click_link "New Recipe"
     expect(page).to have_content("New recipe")
 
-    fill_in "Title", with: "Koppi"
-    fill_in "Coffee", with: "18"
-    fill_in "Water", with: "220"
-    fill_in "Seconds", with: "90"
-    fill_in "Description", with: "My everyday coffee recipe"
+    fill_in "Title", with: options[:title]
+    fill_in "Coffee", with: options[:coffee]
+    fill_in "Water", with: options[:water]
+    fill_in "Seconds", with: options[:seconds]
+    fill_in "Description", with: options[:description]
     click_button "Create Recipe"
+  end
+
+  it "redirects to the recipe index page on success" do
+    create_recipe
 
     expect(page).to have_content("Koppi")
   end
 
-  it "displays and error when the recipe has no title" do
+  it "displays an error when the recipe has no title" do
     expect(Recipe.count).to eq(0)
 
-    visit "/recipes"
-    click_link "New Recipe"
-    expect(page).to have_content("New recipe")
-
-    fill_in "Title", with: ""
-    fill_in "Coffee", with: "18"
-    fill_in "Water", with: "220"
-    fill_in "Seconds", with: "90"
-    fill_in "Description", with: "My everyday coffee recipe"
-    click_button "Create Recipe"
+    create_recipe title: ""
 
     expect(page).to have_content("error")
     expect(Recipe.count).to eq(0)
 
     visit "/recipes"
     expect(page).to_not have_content("Koppi")
+  end
+
+  it "displays an error when the coffee field is empty" do
+    expect(Recipe.count).to eq(0)
+
+    create_recipe coffee: ""
+
+    expect(page).to have_content("error")
+    expect(Recipe.count).to eq(0)
+
+    visit "/recipes"
+    expect(page).to_not have_content("18")
+  end
+
+  it "displays an error when the water field is empty" do
+    expect(Recipe.count).to eq(0)
+
+    create_recipe water: ""
+
+    expect(page).to have_content("error")
+    expect(Recipe.count).to eq(0)
+
+    visit "/recipes"
+    expect(page).to_not have_content("220")
+    end
+
+  it "displays an error when the seconds field is empty" do
+    expect(Recipe.count).to eq(0)
+
+    create_recipe seconds: ""
+
+    expect(page).to have_content("error")
+    expect(Recipe.count).to eq(0)
+
+    visit "/recipes"
+    expect(page).to_not have_content("90")
+  end
+
+
+  it "displays an error when the description field is empty" do
+    expect(Recipe.count).to eq(0)
+
+    create_recipe description: ""
+
+    expect(page).to have_content("error")
+    expect(Recipe.count).to eq(0)
+
+    visit "/recipes"
+    expect(page).to_not have_content("My everyday coffee recipe")
   end
 end

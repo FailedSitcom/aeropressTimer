7365f74b339c244174944ac004ea9aab4f730932
Samuel Durkin <sam.thomas.durkin@gmail.com>
2014-08-16 20:55:03 +0100

    failure

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140815191759) do
+ActiveRecord::Schema.define(version: 20140816191543) do
+
+  create_table "recipe_items", force: true do |t|
+    t.integer  "recipe_id"
+    t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "recipe_items", ["recipe_id"], name: "index_recipe_items_on_recipe_id"
 
   create_table "recipes", force: true do |t|
     t.text     "title"
@@ -22,5 +31,26 @@ ActiveRecord::Schema.define(version: 20140815191759) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "profile_name"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end

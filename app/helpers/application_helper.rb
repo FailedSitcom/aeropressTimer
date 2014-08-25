dc3e28e45306a5690bb1950fde1da81c80ffd05f
Samuel Durkin <sam.thomas.durkin@gmail.com>
2014-08-25 18:18:02 +0100

    Added title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def title(title)
+    content_for(:title) { "#{title} | " }
+  end
+
   def new_recipe_link
     link_to "Add Recipe", new_recipe_path, class: "icon-new right hide-text"
   end

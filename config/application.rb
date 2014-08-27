f64002b777486f6ce62c6923c4f68fdfa5eee634
Samuel Durkin <sam.thomas.durkin@gmail.com>
2014-08-27 21:11:53 +0100

    Add foundation form builder

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module AeropressTimer
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    config.autoload_paths += %W( #{config.root}/lib )
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
@@ -20,6 +20,7 @@ module AeropressTimer
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w( vendor/modernizr.js )
+    config.action_view.field_error_proc = Proc.new {|html, instance| html}
 
   end
 end

e963d3771d2ae10b1c6d444339547553905d6567
Samuel Durkin <sam.thomas.durkin@gmail.com>
2014-09-01 18:21:16 +0100

    Extract blank slates into partials

--- spec/views/recipes/index.html.erb_spec.rb
+++ spec/views/recipes/index.html.erb_spec.rb
@@ -23,7 +23,7 @@ describe "recipes/index" do
   it "renders a list of recipes" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "ul.recipes li div", :text => "Title".to_s, :count => 2
+    assert_select "ul.recipes li div", :text => "Title".to_s, :count => 4
     #assert_select "tr>td", :text => 1.to_s, :count => 2
     #assert_select "tr>td", :text => 2.to_s, :count => 2
     #assert_select "tr>td", :text => 3.to_s, :count => 2

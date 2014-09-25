48583f6f6669862e0fb5ed5687276816ec5dd62d
Samuel Durkin <sam.thomas.durkin@gmail.com>
2014-09-25 21:49:43 +0100

    name update

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :user do
     first_name "First"
     last_name  "Last"
-    sequence(:email) { |n| "user#{n}@aeropressTimer.com" }
+    sequence(:email) { |n| "user#{n}@coffeefilter.me" }
     password   "password1234"
     password_confirmation   "password1234"
   end

ec85452cbf571d9d6069ee1ad5ee2887d44d7dac
Samuel Durkin <sam.thomas.durkin@gmail.com>
2014-09-29 18:15:45 +0100

    fixed timer functionality

--- spec/views/recipes/index.html.erb_spec.rb
+++ spec/views/recipes/index.html.erb_spec.rb
@@ -5,16 +5,16 @@ describe "recipes/index" do
     assign(:recipes, [
       stub_model(Recipe,
         :title => "Title",
-        #:coffee => 1,
-        #:water => 2,
-        #:seconds => 3,
+        :coffee => 1,
+        :water => 2,
+        :seconds => 3,
         :description => "MyText"
       ),
       stub_model(Recipe,
         :title => "Title",
-        #:coffee => 1,
-        #:water => 2,
-        #:seconds => 3,
+        :coffee => 1,
+        :water => 2,
+        :seconds => 3,
         :description => "MyText"
       )
     ])
@@ -24,9 +24,9 @@ describe "recipes/index" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "ul.recipes li div", :text => "Title".to_s, :count => 4
-    #assert_select "tr>td", :text => 1.to_s, :count => 2
-    #assert_select "tr>td", :text => 2.to_s, :count => 2
-    #assert_select "tr>td", :text => 3.to_s, :count => 2
-    #assert_select "p", :text => "MyText".to_s, :count => 2
+    assert_select "ul.recipes li div", :text => 1.to_s, :count => 2
+    assert_select "ul.recipes li div", :text => 2.to_s, :count => 2
+    assert_select "ul.recipes li div", :text => 3.to_s, :count => 2
+    assert_select "p", :text => "MyText".to_s, :count => 2
   end
 end

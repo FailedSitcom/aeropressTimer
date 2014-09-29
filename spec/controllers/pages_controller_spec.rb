80f6e6f8b44f6a25e649a2878c711d24499c0244
Samuel Durkin <sam.thomas.durkin@gmail.com>
2014-09-29 20:16:40 +0100

    about about page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,13 @@ require 'spec_helper'
 
 describe PagesController do
 
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+  end
+
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'

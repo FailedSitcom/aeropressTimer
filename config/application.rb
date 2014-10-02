4054a8d75010ec5411dad7c7de7b48e55c176f96
Samuel Durkin <sam.thomas.durkin@gmail.com>
2014-10-02 22:17:33 +0100

    fixed restarting bug

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module AeropressTimer
 
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.perform_deliveries = true
-    config.action_mailer.raise_delivery_errors = false
+      config.action_mailer.raise_delivery_errors = false
     config.action_mailer.default :charset => "utf-8"
   end
 end

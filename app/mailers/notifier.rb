8f96ed845b1ef5365f5ffc13553514c120f1bade
Samuel Durkin <sam.thomas.durkin@gmail.com>
2014-10-04 21:31:58 +0100

    fixing merge conflic

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,8 +1,5 @@
 class Notifier < ActionMailer::Base
-<<<<<<< HEAD
 
-=======
->>>>>>> c4f1582847aa778f88d0da0a7f66e578666aa6ca
   default :from => "hello@coffeefilter.me"
 
   def password_reset(user)

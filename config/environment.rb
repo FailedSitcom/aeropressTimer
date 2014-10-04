1bc74c23bcc31434a7281b4f6932d38fccf2ef8d
Samuel Durkin <sam.thomas.durkin@gmail.com>
2014-10-04 21:43:57 +0100

    fixed merge error

--- config/environment.rb
+++ config/environment.rb
@@ -7,8 +7,8 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :address => 'smtp.gmail.com',
   :port => 587,
-  :user_name => ENV[MAIL_USER],
-  :password => ENV[MAIL_PASS],
+  :user_name => ENV['MAIL_USER'],
+  :password => ENV['MAIL_PASS'],
   :authentication => :plain,
   :enable_starttls_auto => true,
 }
